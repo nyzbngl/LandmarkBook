bc80b3fd3a017e74144161c556f8e0d6ae0f2711
Niyazi BİNGÜL <>
2021-02-26 00:16:27 +0300

    first commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Niyazi BİNGÜL on 25.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
